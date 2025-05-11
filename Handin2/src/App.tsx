119dbeae278ca2aa7f2a5429082e46b893113ca9
Michael Dao <202307644@post.au.dk>
2025-05-11 20:17:47 +0200

    feat: reroute to manager or model page

--- Handin2/src/App.tsx
+++ Handin2/src/App.tsx
@@ -1,16 +1,19 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import LoginPage from "./pages/LoginPage"; // Importer login-siden
+import LoginPage from "./pages/LoginPage";
+import ManagerPage from "./pages/ManagerPage";
+import ModelPage from "./pages/ModelPage";
 
 const App: React.FC = () => {
     return (
         <Router>
             <Routes>
                 <Route path="/" element={<LoginPage />} />
+                <Route path="/manager" element={<ManagerPage />} />
+                <Route path="/model" element={<ModelPage />} />
             </Routes>
         </Router>
     );
 };
-
 
 export default App;
